4c6e823f9ba6a2d784cf2e58efa674b9014f3099
giulia <giulia.guerreiro@gmail.com>
2022-06-29 13:56:16 -0300

    script.js - timer set

--- script.js
+++ script.js
@@ -6,6 +6,55 @@ const buttonRain = document.querySelector('.rain-sound');
 const buttonForest = document.querySelector('.forest-sound');
 const buttonCoffeeShop = document.querySelector('.coffee-shop-sound');
 const buttonFireplace = document.querySelector('.fireplace-sound');
+const minutesDisplay = document.querySelector('.minutes');
+const secondsDisplay = document.querySelector('.seconds');
+let minutes = Number(minutesDisplay.textContent);
+let timeOutID;
+
+function countdown() {
+  timeOutID = setTimeout(function() {
+    let seconds = Number(secondsDisplay.textContent);
+    let minutes = Number(minutesDisplay.textContent);
+
+    if(minutes <= 0 && seconds <= 0) {
+      resetTimerDisplay()
+      resetSoundButton();
+      buttonPlay.classList.remove('clicked');
+      return
+    }
+
+    if(seconds <= 0) {
+      seconds = 60;
+      --minutes;
+    }
+
+    minutesDisplay.textContent = String(minutes).padStart(2, "0");
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
+
+    countdown();
+  }, 1000)
+}
+
+function increaseMinutes() {
+  
+  if(minutes >= 50) {
+    return
+  }
+
+  minutes += 5;
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+}
+
+function decreaseMinutes() {
+
+  if(minutes < 5) {
+    return
+  }
+
+  minutes -= 5;
+
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+}
 
 function resetSoundButton() {
   buttonRain.classList.remove('active');
@@ -14,15 +63,29 @@ function resetSoundButton() {
   buttonFireplace.classList.remove('active');
 }
 
+function resetTimerDisplay() {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  secondsDisplay.textContent = String(0).padStart(2, "0");
+}
+
 buttonPlay.addEventListener('click', function() {
   buttonPlay.classList.add('clicked');
-  resetSoundButton();
-  buttonForest.classList.add('active');
+  countdown();
 })
 
 buttonStop.addEventListener('click', function() {
-  resetSoundButton();
+  resetTimerDisplay();
+  resetSoundButton();  
   buttonPlay.classList.remove('clicked');
+  clearTimeout(timeOutID);
+})
+
+buttonIncrease.addEventListener('click', function() {
+  increaseMinutes();
+})
+
+buttonDecrease.addEventListener('click', function() {
+  decreaseMinutes();
 })
 
 buttonForest.addEventListener('click', function() {
